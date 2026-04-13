63b7ca6461d9c6c09865c4cc1a1b076fec107a18
Chenzy <chenzy233@qq.com>
2026-04-13 14:50:56 +0800

    fix version error

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import react from '@vitejs/plugin-react';
 
 function getBase() {
   if (!process.env.GITHUB_ACTIONS) return '/';
-
   const repo = process.env.GITHUB_REPOSITORY || '';
   const repoName = repo.split('/')[1] || '';
   return repoName ? `/${repoName}/` : '/';
