b0f26f9dcabc26e90b6c3f292205fb21f51c0a15
Chenzy <chenzy233@qq.com>
2026-04-13 14:42:53 +0800

    udpate github action

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,15 @@
 ﻿import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+function getBase() {
+  if (!process.env.GITHUB_ACTIONS) return '/';
+
+  const repo = process.env.GITHUB_REPOSITORY || '';
+  const repoName = repo.split('/')[1] || '';
+  return repoName ? `/${repoName}/` : '/';
+}
+
 export default defineConfig({
+  base: getBase(),
   plugins: [react()]
 });
